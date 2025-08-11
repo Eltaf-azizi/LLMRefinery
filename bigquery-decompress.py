3e3a23889437ddd1e1ee8faa4e12d1c17e216a3e
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-11 22:40:32 +0500

    working on the decompress file function

--- bigquery-decompress.py
+++ bigquery-decompress.py
@@ -21,3 +21,23 @@ decompressed_dir = f"{YEAR}_j"
 if not os.path.exists(decompressed_dir):
     os.makedirs(decompressed_dir)
 
+
+# Define a function to decompress a file and move it to the decompressed directory
+def decompress_file(file_path):
+    # if this filepath isn't already in decompressed dir:
+    if os.path.exists(os.path.join(decompressed_dir, os.path.basename(file_path))):
+        print("Already here! Skipping: ", file_path)
+    else:
+        # Decompress the file
+        with open(file_path, 'rb') as f_in:
+            with gzip.open(f_in, 'rb') as f_out:
+                shutil.copyfileobj(f_out, open(os.path.join(decompressed_dir, os.path.basename(file_path)), 'wb'))
+                print("done ", file_path)
+    # Delete the compressed file
+    #os.remove(file_path) #  LOL this is why we test with copies... 
+
+
+# Get a list of all the compressed files in the compressed directory
+compressed_files = [os.path.join(compressed_dir, f) for f in os.listdir(compressed_dir) if os.path.isfile(os.path.join(compressed_dir, f))]
+
+
