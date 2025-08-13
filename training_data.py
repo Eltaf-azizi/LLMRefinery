ccc9810379d70463157e3915b01f42814c511041
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-13 22:09:16 +0500

    building training data

---
+++ training_data.py
@@ -0,0 +1,21 @@
+'''
+Silly-man's multiprocessing :)
+
+Run multiple instances of this script or make it better.
+'''
+
+import sqlite3
+import pickle
+import pandas as pd
+import sqlite3
+import colorama
+
+# create table with unique id column and a text column, called walls1337bot
+
+conn = sqlite3.connect('walls1337bot.db')
+c = conn.cursor()
+# create table if it doesnt exist:
+c.execute('''CREATE TABLE IF NOT EXISTS walls1337bot
+             (id TEXT PRIMARY KEY, train_text TEXT, score INT, length INT)''')
+conn.commit()
+#conn.close()
