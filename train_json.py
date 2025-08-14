75da6ca9235536e863cddcfe020ca2e1c035af2b
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-14 20:50:24 +0500

    working on the train_text

--- train_json.py
+++ train_json.py
@@ -15,3 +15,18 @@ c.execute('''CREATE TABLE IF NOT EXISTS walls1337bot
              (id TEXT PRIMARY KEY, train_text TEXT, score INT, length INT)''')
 conn.commit()
 
+
+# save the train_text column of the database to json formatted like {"sample": "text"}
+
+MIN_SCORE = 7
+MIN_LEN = 5
+MAX_CHARS = 7500
+c.execute("SELECT train_text FROM walls1337bot WHERE score >= ? AND length >= ?", (MIN_SCORE, MIN_LEN))
+rows = c.fetchall()
+print('Number of samples with these settings:',len(rows))
+
+# save each row of train_text to json file such that it looks like
+# {"sample": "text"} per line
+
+
+
