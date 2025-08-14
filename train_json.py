f3176f028b4b272b339d5857cae52f76b5c398fd
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-14 20:52:00 +0500

    working on the bad contents and conditions

--- train_json.py
+++ train_json.py
@@ -30,3 +30,26 @@ print('Number of samples with these settings:',len(rows))
 
 
 
+bad_contents = ["[deleted]", 
+                "[removed]", 
+                "I am a bot, and this action was performed automatically. Please contact the moderators of this subreddit if you have any questions or concerns.",
+                ".jpg",".png",".jpeg",".gif",
+                ]
+
+
+
+with open(f'WSB-min_score-{MIN_SCORE}-min_len-{MIN_LEN}-002.json', 'w') as f:
+
+    for row in rows:
+        
+        if len(row[0]) < MAX_CHARS:
+
+            if not any(bad in row[0] for bad in bad_contents):
+                f.write(json.dumps({"sample": row[0]}) + "\n")
+
+            else:
+                print('bad content found')
+
+
+conn.close()
+
